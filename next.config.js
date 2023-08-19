cf50a20863bdb39cd9d4d63965ce6f8e8620fdbb
sayed sifat <sayedhossainsifat100@gmail.comm>
2023-08-19 06:45:16 +0600

    just get some change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+// another combine
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first")
 const nextConfig = {
   reactStrictMode: true,
   forceSwcTransforms: true,
