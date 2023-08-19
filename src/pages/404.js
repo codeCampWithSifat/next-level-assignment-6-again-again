98b7ce4275bee485455369f4e22e143f96ba4e3b
sayed sifat <sayedhossainsifat100@gmail.comm>
2023-08-19 06:01:24 +0600

    just added some code

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 const NotFound = () => {
   const router = useRouter();
 
+ useEffect(() => {
   setTimeout(() => {
     router.push("/");
   }, 2000);
+ },[])
   return (
     <div className="flex justify-center items-center min-h-screen">
       <h1 className="text-5xl text-violet-900">Page Not Found..............</h1>
